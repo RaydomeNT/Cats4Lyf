81e3f82c84b3a488167a98d7a82dfb94603d5a8d
RaydomeNT <raydome85@gmail.com>
2022-07-13 10:06:39 +0100

    test1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
 import './App.css';
 
-const App = () => {
+const App  = () =>  { 
+  const [item, setItem] = useState("");
+
+  const getData = async () => {
+    let response = await fetch("https://api.thecatapi.com/v1/images/search");
+    let data = await response.json();
+    setItem(data[0]);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div>
-      
-    </div>
+    <body>
+      <h1>Which cat will you take home?</h1>
+      <h2>{item.id}</h2>
+      <img src={item.url} alt='cat' />
+      {item ? (
+        item.breeds.map((breeds, index) => {
+          return <Breeds breeds={breeds} />;
+        })
+      ) : (
+        <p>loading...</p>
+      )}
+      <button onClick={getData}>Choose another</button>
+    </body>
   );
-}
+};
+
+const Breeds = ({ breeds }) => {
+  return (
+      <p>{item.breeds}</p>
+  );
+};
 
 export default App;
