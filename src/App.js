6b0cd0aa62638bd90e8adad0db91580af07a6d4e
RaydomeNT <raydome85@gmail.com>
2022-07-13 15:03:54 +0100

    faker info in app js

--- src/App.js
+++ src/App.js
@@ -8,38 +8,23 @@ const App  = () =>  {
   const getData = async () => {
     let response = await fetch("https://api.thecatapi.com/v1/images/search");
     let data = await response.json();
-    // const faker = require('faker');
-    // const randomName = faker.name,firsName();
     setItem(data[0]);
   };
   useEffect(() => {
     getData();
   }, []);
+
   return (
-    <body>
-      <h1>Which cat will you take home?</h1>
-      <img src={item.url} alt='cat' height='600'/>
-      {/* {item ? (
-        item.breeds.map((breeds, index) => {
-          return <Breeds breeds={breeds} />;
-        }) */}
-       {/* :  */}
-        {/* <p>loading...</p> */}
-      
-      <p>Name: {faker.name.firstName()} '{faker.lorem.word()}' {faker.name.lastName()}</p>
-      <p>Age: {faker.datatype.number(18)}</p>
-      <p>Breed: {faker.animal.cat()}</p>
-      <p>Job: {faker.name.jobType()}</p>
-      <p>Description: {faker.commerce.productDescription()}</p>
-      <button onClick={getData}>Add to cart: £{faker.commerce.price()}</button>
-    </body>
-  );
+    <div>
+        <img src={item.url} alt='cat' height='200'/>         
+        <p>Name: {faker.name.firstName()} '{faker.lorem.word()}' {faker.name.lastName()}</p>
+        <p>Age: {faker.datatype.number(18)}</p>
+        <p>Breed: {faker.animal.cat()}</p>
+        <p>Job: {faker.name.jobType()}</p>
+        <p>Description: {faker.commerce.productDescription()}</p>
+        <button onClick={getData}>Add to cart: £{faker.commerce.price()}</button>
+    </div>
+    );
 };
-
-// const Breeds = ({ breeds }) => {
-//   return (
-//       <p>{breeds}</p>
-//   );
-// };
-
+    
 export default App;
